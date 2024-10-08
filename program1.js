1f79e77b7eda6ec562b93a8f3d413c43abfdf3fb
harhit-1000 <harshitredcolour@gmail.com>
2024-10-08 14:13:53 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,22 +3,7 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-  const char = input[i];
-
-  if (map[char]) {
-    // Push the corresponding closing parenthesis onto the stack
-    stack.push(map[char]);
-  } else if (char === ')' || char === '}' || char === ']') {
-    // Check if the current closing parenthesis matches the top of the stack
-    if (stack.pop() !== char) {
-      return false;
-    }
-  }
-}
-
-// If the stack is empty, all parentheses were properly closed
-return stack.length === 0;
-
+    
 };
 
 module.exports = { isValid };
