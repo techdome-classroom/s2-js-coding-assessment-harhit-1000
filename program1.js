813ba48d6b93eb9dc0dbbf9705da045a4f9d8d47
harhit-1000 <harshitredcolour@gmail.com>
2024-10-08 14:14:53 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,7 +3,12 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+  const stack = [];
+  const map = {
+    '(': ')',
+    '{': '}',
+    '[': ']'
+  };
 };
 
 module.exports = { isValid };
