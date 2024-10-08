0d81327ca05ae1a765e2362b5bac33a61daedc32
harhit-1000 <harshitredcolour@gmail.com>
2024-10-08 14:15:19 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -9,6 +9,23 @@ var isValid = function(s) {
     '{': '}',
     '[': ']'
   };
+
+  for (let i = 0; i < input.length; i++) {
+    const char = input[i];
+
+    if (map[char]) {
+      // Push the corresponding closing parenthesis onto the stack
+      stack.push(map[char]);
+    } else if (char === ')' || char === '}' || char === ']') {
+      // Check if the current closing parenthesis matches the top of the stack
+      if (stack.pop() !== char) {
+        return false;
+      }
+    }
+  }
+
+  // If the stack is empty, all parentheses were properly closed
+  return stack.length === 0;
 };
 
 module.exports = { isValid };
